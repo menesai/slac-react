d4ad0cb32b75ba1fd0c2b6c2ffe9853a2a23bfe9
Uriel Meneses <saidmeneses1@live.com>
2020-02-03 21:35:17 -0600

    hosting changes

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import firebase from "../../firebase";
-import {
-  Grid,
-  Form,
-  Segment,
-  Button,
-  Header,
-  Message,
-  Icon
-} from "semantic-ui-react";
+import {Grid,Form,Segment,Button,Header,Message,Icon} from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 class Login extends React.Component {
